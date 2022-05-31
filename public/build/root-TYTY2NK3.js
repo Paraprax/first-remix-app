ad23a63955e04410d42bb997b087e93419f2333d
Paraprax <26296581+Paraprax@users.noreply.github.com>
2022-05-31 11:19:45 -0400

    Rewrite links in index.jsx to internal placeholders

---
+++ public/build/root-TYTY2NK3.js
@@ -0,0 +1,33 @@
+import {
+  Links,
+  LiveReload,
+  Meta,
+  Outlet,
+  Scripts,
+  ScrollRestoration
+} from "/build/_shared/chunk-NNVKAHH7.js";
+import {
+  React,
+  init_react
+} from "/build/_shared/chunk-IYRIQ6PI.js";
+
+// browser-route-module:/Users/Parker/Programming/Projects/react/remix-react/my-remix-app/first-remix-app/app/root.jsx?browser
+init_react();
+
+// app/root.jsx
+init_react();
+var meta = () => ({
+  charset: "utf-8",
+  title: "New Remix App",
+  viewport: "width=device-width,initial-scale=1"
+});
+function App() {
+  return /* @__PURE__ */ React.createElement("html", {
+    lang: "en"
+  }, /* @__PURE__ */ React.createElement("head", null, /* @__PURE__ */ React.createElement(Meta, null), /* @__PURE__ */ React.createElement(Links, null)), /* @__PURE__ */ React.createElement("body", null, /* @__PURE__ */ React.createElement(Outlet, null), /* @__PURE__ */ React.createElement(ScrollRestoration, null), /* @__PURE__ */ React.createElement(Scripts, null), /* @__PURE__ */ React.createElement(LiveReload, null)));
+}
+export {
+  App as default,
+  meta
+};
+//# sourceMappingURL=/build/root-TYTY2NK3.js.map
---
+++ public/build/routes/index-DZ6JKCMZ.js
@@ -0,0 +1,24 @@
+import {
+  Link,
+  React,
+  init_react
+} from "/build/_shared/chunk-HS2QE3UK.js";
+
+// browser-route-module:/Users/Parker/Programming/Projects/react/remix-react/my-remix-app/first-remix-app/app/routes/index.jsx?browser
+init_react();
+
+// app/routes/index.jsx
+init_react();
+function Index() {
+  return /* @__PURE__ */ React.createElement("div", {
+    style: { fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }
+  }, /* @__PURE__ */ React.createElement("h1", null, "Welcome to Remix"), /* @__PURE__ */ React.createElement("ul", null, /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement(Link, {
+    to: "/one"
+  }, "My Link 1")), /* @__PURE__ */ React.createElement("li", null, /* @__PURE__ */ React.createElement(Link, {
+    to: "/two"
+  }, "My Link 2"))));
+}
+export {
+  Index as default
+};
+//# sourceMappingURL=/build/routes/index-DZ6JKCMZ.js.map
